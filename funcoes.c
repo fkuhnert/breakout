f12ef134af00585e3b151cb58df35c8073462698
Unknown <fvk98@yahoo.com.br>
2017-06-23 01:02:51 -0300

    Screen name

--- funcoes.c
+++ funcoes.c
@@ -103,7 +103,7 @@ int init() {
     }
     else {
         /*Create window*/
-        gWindow = SDL_CreateWindow( "SDL Tutorial: 06_moving_images", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+        gWindow = SDL_CreateWindow( "Breakout!", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
         if( gWindow == NULL ) {
             printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
             success = false;
